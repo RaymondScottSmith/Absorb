4cbf038149be414de3abc77753b745b6a5f1d995
RaymondScottSmith <rayscottsmith1@gmail.com>
2022-08-12 16:52:46 -0500

    Added comments to playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,20 +25,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //When mouse button released
         if (Input.GetMouseButtonUp(0) )
         {
+            //Check readyToLaunch to make sure it doesn't automatically launch when we release click after stopping
             if (readyToLaunch)
             {
                 readyToLaunch = false;
                 moving = true;
+                //Get Mouse position
                 var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+                //Calculate direction to mouse from gameobject
                 var mouseDir = mousePos - gameObject.transform.position;
                 mouseDir.z = 0.0f;
+                //Normalize the direction to make sure distance does not affect speed
                 mouseDir.Normalize();
+                //Start the player moving
                 rb.AddForce(mouseDir * baseSpeed, ForceMode2D.Impulse);
             }
             else if (!moving)
             {
+                //Resets player when releasing mouse button after sticking
                 readyToLaunch = true;
             }
         }
@@ -47,14 +54,19 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
+        //If we hit a bounceable object while holding down mouse and moving
         if (col.gameObject.CompareTag("Bounceable") && Input.GetMouseButton(0) && moving)
         {
+            //Calculate reverse of current velocity
             Vector2 reverseVelocity = -rb.velocity;
+            //Set current velocity to zero. Stopping the player
             rb.velocity = Vector2.zero;
             moving = false;
+            //Use raycast to determine exactly where we impacted wall
             RaycastHit2D hit = Physics2D.Raycast(transform.position, reverseVelocity);
             if (hit.collider != null)
             {
+                //Move the player back if we bounced a little away from the wall
                 transform.position = hit.point;
             }
         }
