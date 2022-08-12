e8979a9a3c0c0ad650695beff98b4b44d6169eaa
RaymondScottSmith <rayscottsmith1@gmail.com>
2022-08-12 16:46:55 -0500

    Bouncing and sticking fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,21 +9,55 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float baseSpeed = 1;
 
     private Rigidbody2D rb;
+    private bool moving;
+    private bool touchingBounceable;
+    private bool readyToLaunch;
+    public PhysicsMaterial2D bouncyMaterial;
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        moving = false;
+        readyToLaunch = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0) )
         {
-            var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-            var mouseDir = mousePos - gameObject.transform.position;
-            mouseDir = mouseDir.normalized;
-            rb.AddForce(mouseDir * baseSpeed, ForceMode2D.Impulse);
+            if (readyToLaunch)
+            {
+                readyToLaunch = false;
+                moving = true;
+                var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+                var mouseDir = mousePos - gameObject.transform.position;
+                mouseDir.z = 0.0f;
+                mouseDir.Normalize();
+                rb.AddForce(mouseDir * baseSpeed, ForceMode2D.Impulse);
+            }
+            else if (!moving)
+            {
+                readyToLaunch = true;
+            }
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Bounceable") && Input.GetMouseButton(0) && moving)
+        {
+            Vector2 reverseVelocity = -rb.velocity;
+            rb.velocity = Vector2.zero;
+            moving = false;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, reverseVelocity);
+            if (hit.collider != null)
+            {
+                transform.position = hit.point;
+            }
         }
     }
+
 }
