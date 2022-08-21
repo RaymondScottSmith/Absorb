76295bed56d6399f77623beed3ff2aa6d0772748
RaymondScottSmith <rayscottsmith1@gmail.com>
2022-08-21 16:04:08 -0500

    Setup timeline animations for camera, laser, runner and UI

---
+++ Assets/Scripts/TimelineManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineManager : MonoBehaviour
+{
+    private bool isFinished = false;
+
+    private PlayerController player;
+    [SerializeField] private PlayableDirector director;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isFinished && director.state != PlayState.Playing)
+        {
+            isFinished = true;
+            player.readyToPlay = true;
+        }
+    }
+}
